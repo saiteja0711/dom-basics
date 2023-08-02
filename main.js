653e7d86b803b5e79e39c0928d0c89890fd2f335
saiteja0711 <saiteja7112001@gmail.com>
2023-08-02 23:00:37 +0530

    firstcommit

---
+++ main.js
@@ -0,0 +1,44 @@
+// const ul=document.querySelector('.items');
+// ul.firstElementChild.innerHTML='<h1>Hello</h1>';
+// ul.firstElementChild.style.color='green';
+// ul.children[1].style.color='yellow';
+// const form=document.querySelector('#myform');
+// const name=document.querySelector('#name');
+// const email=document.querySelector('#email');
+// const msg=document.querySelector('.msg');
+// const user=document.querySelector('#users');
+
+// const btn=document.querySelector('.btn');
+
+// btn.addEventListener('mouseout',(e)=>{
+//     e.preventDefault();
+//     document.querySelector('#my-form').style.background='#ccc';
+//     document.querySelector('body').classList.add('bg-dark');
+    
+    
+
+// });
+const form=document.querySelector('#my-form');
+const inpName=document.querySelector('#name');
+const inpEmail=document.querySelector('#email');
+const msg=document.querySelector('.msg');
+const user=document.querySelector('#users');
+
+form.addEventListener('submit',onSubmit);
+function onSubmit(e){
+    e.preventDefault();
+    if(inpName.value==''||inpEmail=='')
+    {
+        msg.classList.add('error');
+        msg.textContent='Add some valid Input';
+        setTimeout(()=> msg.remove(),700);
+    }
+    else{
+        const li=document.createElement('li');
+        li.appendChild(document.createTextNode(`${inpName.value}:${inpEmail.value}`));
+        user.appendChild(li);
+
+    }
+    inpEmail.value='';
+    inpName.value='';
+}
